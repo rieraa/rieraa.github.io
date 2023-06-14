76e154b7d0fe934f2fbee652e9b134cb13f18504
oooo <ameee9@163.com>
2023-06-14 18:22:47 +0800

    pure

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -32,7 +32,7 @@ export default hopeTheme({
   displayFooter: true,
 
   blog: {
-    description: "一个前端开发者",
+    description: "一个后端开发者",
     intro: "/intro.html",
     medias: {
       Baidu: "https://example.com",
